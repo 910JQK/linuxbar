21fbbf3fd1122005484f78d0fd2866e717c47c88
910JQK <v910JQK@gmail.com>
2016-07-25 21:07:38 +0800

    Validation: check sha256 using regex; add email validation

--- validation.py
+++ validation.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 
-HEXDIGITS = '0123456789abcdef'
+import re
+
+
+# Whether an email address is valid is finally checked by the activation mail
+email_regex = re.compile('[^@]+@[^@]+')
+sha256_regex = re.compile('[0123456789abcdef]{64}')
 
 
 class ValidationError(Exception):
@@ -12,7 +17,7 @@ def _(string):
     return string # reserved for l10n
 
 
-def validate(field, string, min=0, max=0, not_empty=False):
+def validate(field, string, min=0, max=0, not_empty=False, regex=None):
     size = len(string.encode('utf8'))
     if(min and size < min):
         raise ValidationError(_(
@@ -24,13 +29,13 @@ def validate(field, string, min=0, max=0, not_empty=False):
         ))
     if(not_empty and size == 0):
         raise ValidationError(_('%s should not be empty.' % field))
+    if(regex and not regex.fullmatch(string)):
+        raise ValidationError(_('%s: Wrong format.' % field))
+
+
+def validate_email(field, string):
+    validate(field, string, regex=email_regex)
 
 
 def validate_sha256(field, string):
-    if(len(string) != 64):
-        raise ValidationError(_('%s should be a valid sha256 string.' % field))
-    for char in string:
-        if(char not in HEXDIGITS):
-            raise ValidationError(_(
-                '%s should be a valid sha256 string.' % field
-            ))
+    validate(field, string, regex=sha256_regex)
