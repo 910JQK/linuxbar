113c1c52b822ae83ae10177d0de43b27bb207800
910JQK <v910JQK@gmail.com>
2016-08-02 21:18:37 +0800

    Toolkit: add strlen() and utf8sizeof()

--- static/toolkit.js
+++ static/toolkit.js
@@ -92,3 +92,36 @@ function POST(url, data, ok, err, timeout) {
     xhr.send(params);
     return xhr;
 }
+
+
+/**
+ * Return the real length of a string
+ *
+ * @param String string
+ * @return Number
+ */
+function strlen(string) {
+    return Array.from(string).length;
+}
+
+
+/**
+ * Return the count of bytes (in UTF-8) of a string
+ *
+ * @param String string
+ * @return Number
+ */
+function utf8sizeof(string) {
+    /* JavaScript uses UCS-2, not supporting 4-byte UTF-16 characters. */
+    var s = string.length;
+    for(var i=string.length-1; i>=0; i--) {
+	var code = string.charCodeAt(i);
+	if(code > 0x7f && code <= 0x7ff)
+	    s++;
+	else if(code > 0x7ff && code <= 0xffff)
+	    s += 2;
+	if(code >= 0xDC00 && code <= 0xDFFF)
+	    i--;
+    }
+    return s;
+}
