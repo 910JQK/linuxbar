9a609c917fdf0c62c0e951a0420f509cf0f71ba3
910JQK <v910JQK@gmail.com>
2016-08-03 17:05:53 +0800

    Toolkit: add validate_size() and init_size_validation()

--- static/toolkit.js
+++ static/toolkit.js
@@ -1,3 +1,7 @@
+var query = selector => document.querySelector(selector);
+var query_all = selector => document.querySelectorAll(selector);
+
+
 /* Reserved for l10n */
 function _(string) {
     return string;
@@ -114,4 +118,43 @@ function strlen(string) {
 function utf8sizeof(string) {
     /* JavaScript uses UCS-2, not supporting 4-byte UTF-16 characters. */
     return unescape(encodeURIComponent(string)).length;
+}
+
+
+/**
+ * Check if the UTF-8 size of the value of a <input> is in appropriate range. 
+ * Change event handler for form validation.
+ *
+ * @this HTMLInputElement
+ * @return void
+ */
+function validate_size() {
+    var size = utf8sizeof(this.value);
+    var min = Number.parseInt(this.dataset.min);
+    var max = Number.parseInt(this.dataset.max);
+    if(size < min)
+	this.setCustomValidity(
+	    printf(_('%1 should be at least %2 bytes.'), this.placeholder, min)
+	);
+    else if(size > max)
+	this.setCustomValidity(
+	    printf(_('%1 should be at most %2 bytes.'), this.placeholder, max)
+	);
+    else
+	this.setCustomValidity('');
+}
+
+
+/**
+ * Add event handlers for size-type form validation.
+ *
+ * @return void
+ */
+function init_size_validation() {
+    for(let input of query_all('input[data-min], input[data-max]')) {
+	input.addEventListener('change', validate_size);
+	/* The browser may save the previous data. */
+	if(input.value)
+	    validate_size.call(input);
+    }
 }
