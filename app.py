efe7fa578f9e76fe7811b5848cd9bfb8905b4e15
910JQK <v910JQK@gmail.com>
2016-08-05 21:44:02 +0800

    App: format_date(): impl

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ import io
 import re
 import json
 import smtplib
+import datetime
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -33,8 +34,9 @@ class ForumPermissionCheckError(Exception):
     pass
 
 
+# reserved for l10n
 def _(string):
-    return string # reserved for l10n
+    return string
 
 
 def send_mail(subject, addr_from, addr_to, content, html_content=''):
@@ -120,6 +122,53 @@ def permission_err_response(err):
 @app.context_processor
 def inject_config():
     return dict(config=config)
+
+
+@app.template_filter('date')
+def format_date(timestamp):
+    date = datetime.datetime.fromtimestamp(timestamp)
+    delta = round((datetime.datetime.now() - date).total_seconds())
+    if(delta < 60):
+        return _('just now')
+    elif(delta < 3600):
+        minutes = delta // 60
+        if(minutes == 1):
+            return _('a minute ago')
+        else:
+            return _('%d minutes ago') % minutes
+    elif(delta < 86400):
+        hours = delta // 3600
+        if(hours == 1):
+            return _('an hour ago')
+        else:
+            return _('%d hours ago') % hours
+    # 604800 = 86400*7
+    elif(delta < 604800):
+        days = delta // 86400
+        if(days == 1):
+            return _('a day ago')
+        else:
+            return _('%d days ago') % days
+    # 2629746 = 86400*(31+28+97/400+31+30+31+30+31+31+30+31+30+31)/12
+    elif(delta < 2629746):
+        weeks = delta // 604800
+        if(weeks == 1):
+            return _('a week ago')
+        else:
+            return _('%d weeks ago') % weeks
+    # 31556952 = 86400*(365+97/400)
+    elif(delta < 31556952):
+        months = delta // 2629746
+        if(months == 1):
+            return _('a month ago')
+        else:
+            return _('%d months ago') % months
+    else:
+        years = delta // 31556952
+        if(years == 1):
+            return _('a year ago')
+        else:
+            return _('%d years ago') % years
 
 
 @app.route('/')
