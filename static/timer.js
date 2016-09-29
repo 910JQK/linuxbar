483d5d06b6042731e603cf7001fba3e08aa4fe42
910JQK <v910JQK@gmail.com>
2016-09-29 21:17:49 +0800

    Timer: add timer to update unread count

--- static/timer.js
+++ static/timer.js
@@ -1,4 +1,5 @@
 const INTERVAL_UPDATE_DATE = 60*1000;
+const INTERVAL_UPDATE_UNREAD_COUNT = 150*1000;
 
 
 function message(msg) {
@@ -9,3 +10,4 @@ function message(msg) {
 
 
 setInterval(message('update_date'), INTERVAL_UPDATE_DATE);
+setInterval(message('update_unread_count'), INTERVAL_UPDATE_UNREAD_COUNT);
