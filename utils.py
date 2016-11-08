ee61feae80cd6635f3ee26c856560947d7c86694
910JQK <v910JQK@gmail.com>
2016-11-08 16:10:05 +0800

    Utils: Add now(), gen_token(), sha256(), send_mail()

--- utils.py
+++ utils.py
@@ -1,2 +1,45 @@
+import hashlib
+import datetime
+
+
+now = datetime.datetime.now
+
+
 # reserved for l10n
-_ = lambda string: string
+def _(string):
+    return string
+
+
+def sha256(string):
+    return hashlib.sha256(bytes(string, encoding='utf8')).hexdigest()
+
+
+def gen_token():
+    return ''.join(random.choice(TOKEN_CHARS) for i in range(0, 16))
+
+
+class EmailThread(threading.Thread):
+    def __init__(self, subject, addr_from, addr_to, content, html=''):
+        if(html):
+            msg = MIMEMultipart('alternative')
+            msg_plaintext = MIMEText(content, 'plain')
+            msg_html = MIMEText(html, 'html')
+            msg.attach(msg_html)
+            msg.attach(msg_plaintext)
+            msg = msg
+        else:
+            msg = MIMEText(content, 'plain')
+        msg['Subject'] = subject
+        msg['From'] = addr_from
+        msg['To'] = addr_to
+        self.msg = msg
+        threading.Thread.__init__(self)
+
+    def run (self):
+        smtp = smtplib.SMTP('localhost')
+        smtp.send_message(self.msg)
+        smtp.quit()
+
+
+def send_mail(subject, addr_from, addr_to, content, html=''):
+    EmailThread(subject, addr_from, addr_to, content, html).start()
