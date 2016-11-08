8b26c2c05fe8bac650be1b3cb91b76b11f2d3309
910JQK <v910JQK@gmail.com>
2016-11-08 22:06:31 +0800

    App: create basic test app

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
 #!/usr/bin/env python3
 
 
+import os
 from flask import Flask
 from flask_babel import Babel
+from flask_login import current_user
 
 
+from models import Config
 from user import user, login_manager
 
 
 app = Flask(__name__)
 babel = Babel(app)
 login_manager.init_app(app)
-app.register_blueprint(user)
+app.register_blueprint(user, url_prefix='/user')
+DEBUG = True
+
+
+@app.context_processor
+def inject_data():
+    def get_config(name):
+        return Config.get(name)
+    return dict(get_config=get_config)
 
 
 @app.route('/')
 def index():
     # just for testing
-    html = '<!DOCTYPE html><h1>It just works, but very ugly.</h1>'
+    html = '<!DOCTYPE html><title>Test</title><h1>It just works, but very ugly.</h1>'
     if current_user.is_authenticated:
+        html += '<div><span>%d</span><span>%s</span><a href="/user/logout">Logout</a></div>' % current_user.id, current_user.name
+    else:
         html += '<div><a target="_blank" href="/user/register">Register</a></div>'
         html += '<div><a target="_blank" href="/user/login">Sign in</a></div>'
-    else:
-        html += '<div><span>%d</span><span>%s</span><a href="/user/logout">Logout</a></div>' % current_user.id, current_user.name
     return html
+
+
+def main():
+    app.secret_key = os.urandom(24)
+    app.run(debug=DEBUG)
+
+
+if __name__ == '__main__':
+    main()
