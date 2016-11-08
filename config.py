2df8dd79af5083276ba32841a746cc7e530fef9d
910JQK <v910JQK@gmail.com>
2016-11-08 16:10:16 +0800

    Config: add

---
+++ config.py
@@ -0,0 +1,12 @@
+DEFAULT_CONFIG = {
+    'site_name': 'Linuxbar',
+    'site_url': 'http://127.0.0.1:5000',
+    'count_topic': '30',
+    'count_post': '25',
+    'count_subpost': '10',
+    'count_cut': '4',
+    'count_item': '15'
+}
+
+
+PINNED_TOPIC_MAX = 5
