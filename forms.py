a1b0ed2ffb9d75a69ff3ef9b67e1dbec71fa582e
910JQK <v910JQK@gmail.com>
2016-11-08 20:47:27 +0800

    Forms: add

---
+++ forms.py
@@ -0,0 +1,108 @@
+from flask_wtf import FlaskForm
+from wtforms import *
+from wtforms.validators import *
+
+
+from utils import _
+from validation import *
+from captcha import CAPTCHA_REGEX
+
+
+def Required():
+    return InputRequired(_('This field is required.'))
+
+
+def Username():
+    return Regexp(REGEX_USERNAME, message=_('Invalid username format.'))
+
+
+def PasswordConfirm():
+    return EqualTo('password', message=_('Passwords are inconsistent.'))
+
+
+class RegisterForm(FlaskForm):
+    mail = StringField(
+        _('Email Address'),
+        validators = [
+            Required(), Email(_('Invalid Email Address.')), SizeRange(3, 64)
+        ],
+        description = _('Used to enable your account and reset your password.')
+    )
+    name = StringField(
+        _('Nickname'),
+        validators = [
+            Required(), Username()
+        ],
+        description = _('Nickname as well as identifier.')
+    )
+    password = PasswordField(
+        _('Password'),
+        validators = [
+            Required()
+        ],
+        description = _("Your password. Won't be encrypted if not using HTTPS.")
+    )
+    confirm = PasswordField(
+        _('Password Confirmation'),
+        validators = [
+            PasswordConfirm()
+        ],
+        description = _('Repeat your password to confirm.')
+    ])
+    captcha = StringField(
+        _('Captcha'),
+        validators = [
+            Required(), Regexp(CAPTCHA_REGEX, message=_('Invalid captcha'))
+        ]
+    )
+
+
+class LoginForm(FlaskForm):
+    login_name = StringField(
+        _('Login name'),
+        validators = [
+            Required(), SizeRange(3, 64)
+        ],
+        description = _('Login Name can be either nickname or email address.')
+    )
+    password = PasswordField(
+        _('Password'),
+        validators = [
+            Required()
+        ],
+        description = _("Your password. Won't be encrypted if not using HTTPS.")
+    )
+
+
+class GetTokenForm(FlaskForm):
+    mail = StringField(
+        _('Email Address'),
+        validators = [
+            Required(), Email(_('Invalid Email Address.'))
+        ],
+        description = _('Email address of your account')
+    )
+
+
+class PasswordResetForm(FlaskForm):    
+    password = PasswordField(
+        _('New Password'),
+        validators = [
+            Required()
+        ],
+        description = _("New password. Won't be encrypted if not using HTTPS.")
+    )
+    confirm = PasswordField(
+        _('Password Confirmation'),
+        validators = [
+            PasswordConfirm()
+        ],
+        description = _('Repeat your password to confirm.')
+    ])
+    token = StringField(
+        _('Token'),
+        validators = [
+            Regexp(REGEX_TOKEN, message=_('Invalid token format'))
+        ],
+        description = _('The token sent to you by email.')
+    )
