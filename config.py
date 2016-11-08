0e78c98e732743e65a7fc29313074aa8ebeb7608
910JQK <v910JQK@gmail.com>
2016-11-08 19:29:38 +0800

    Config: add mail_addr to DEFAULT_CONFIG

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DEFAULT_CONFIG = {
     'site_name': 'Linuxbar',
     'site_url': 'http://127.0.0.1:5000',
+    'mail_addr': 'no_reply@foo.bar',
     'count_topic': '30',
     'count_post': '25',
     'count_subpost': '10',
