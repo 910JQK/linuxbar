f426ddeaeffbae3f890dd7e67857a561c7ace42a
910JQK <v910JQK@gmail.com>
2016-12-21 22:44:54 +0800

    Add switch button to show distillate topics only

--- forum.py
+++ forum.py
@@ -31,7 +31,7 @@ forum = Blueprint(
 def topic_list(tag_slug):
     pn = int(request.args.get('pn', '1'))
     count = int(Config.Get('count_topic'))
-    distillate_only = bool(request.args.get('distillate', ''))
+    distillate_only = bool(request.args.get('dist', ''))
     distillate_condition = (not distillate_only or Topic.is_distillate == True)
     if not tag_slug:
         is_index = True
